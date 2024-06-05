b446322c32b4fb975c7356817f657fbe5f36ddb0
anuroop <anuroop@metrictreelabs.com>
2024-06-05 17:03:39 +0530

    link change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client"
 
-const socket = socketIO.connect("https://a8d2-115-245-212-236.ngrok-free.app");
+const socket = socketIO.connect("https://socket.backslashgo.com");
 
 function App() {
   return (
